e8da0c4b40f7b7da4d647b3f58995fc08f4f6577
django <12345@qq.com>
2025-05-24 01:41:07 -0700

    模板

--- bookmanager/book/views.py
+++ bookmanager/book/views.py
@@ -1,3 +1,10 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+def index(request):
+    context = {
+        'name':'hello'
+    }
+    # return HttpResponse("Hello, world. You're at the polls index.")
+    return render(request, 'book/index.html',context=context)
